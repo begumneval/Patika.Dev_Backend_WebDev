28261b71d44306b0f27b236ed9968132850be8d2
Begüm Neval Yılmaz <begumnevalylmz@gmail.com>
2025-03-15 22:24:12 +0300

    Initial commit

---
+++ src/main/java/PatikaDev/Hafta1/StarTriangle.java
@@ -0,0 +1,27 @@
+package PatikaDev.Hafta1;
+
+import java.util.Scanner;
+
+public class StarTriangle {
+    public static void main(String[] args) {
+
+        Scanner scanner= new Scanner(System.in);
+
+        //kullanıcıdan basamak sayısını alan kod bloğu
+        System.out.print("Basamak sayısını giriniz: ");
+        int num = scanner.nextInt();
+
+        /* ilk olarak basamak sayısı kadar * yazdırıp birer azaltarak
+        alt satıra geçen patterni oluşturmak için kullanılan iç içe döngü */
+        for (int i = num; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+
+            }
+            //alt satıra geçme işlemi burada yapılıyor.
+            System.out.println();
+
+        }
+
+    }
+}
