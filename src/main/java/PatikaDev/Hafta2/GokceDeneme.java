f63408c3b17c895aed0e6b3b7f407f3eac637864
Begüm Neval Yılmaz <begumnevalylmz@gmail.com>
2025-03-20 12:48:57 +0300

    gökçe deneme

--- src/main/java/PatikaDev/Hafta2/GokceDeneme.java
+++ src/main/java/PatikaDev/Hafta2/GokceDeneme.java
@@ -2,6 +2,6 @@ package PatikaDev.Hafta2;
 
 public class GokceDeneme {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World2");
     }
 }
